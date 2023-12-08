903c030c1eb31e9d489c480599efd50837c038d5
koki428 <koki.kikkawa@gmail.com>
2023-12-09 01:18:13 +0900

    マップを追加”

--- resources/views/stores/show.blade.php
+++ resources/views/stores/show.blade.php
@@ -16,7 +16,7 @@
                 <p>ホットペッパー　{{ $store->hotpeppar }}</p>
                 <p>食べログ　{{ $store->tabelog }}</p>
                 <p>Google　{{ $store->google }}</p>
-            </div>
+        </div>
         <div class="store_detail">
             <div class="time">
                 <h3>営業時間</h3>
@@ -42,6 +42,41 @@
                 <h3>住所</h3>
                 <p>{!! nl2br(e($store->address)) !!}</p>
             </div>
+            <div id="map" style="height:400px; width:600px"></div>
+            <script>const point = @json($store->address)</script>
+            <script type="text/javascript">　//マップを表示
+            function initMap() {
+                var target = document.getElementById('map'); 
+                var address = point;
+                var geocoder = new google.maps.Geocoder();  
+
+                geocoder.geocode({ address: address }, function(results, status){
+                    if (status === 'OK' && results[0]){
+
+                    console.log(results[0].geometry.location);
+
+                    var map = new google.maps.Map(target, {  
+                        center: results[0].geometry.location,
+                        zoom: 18
+                    });
+
+                    var marker = new google.maps.Marker({
+                        position: results[0].geometry.location,
+                        map: map,
+                        animation: google.maps.Animation.DROP
+                    });
+
+                    }else{ 
+                    //住所が存在しない場合の処理
+                    alert('住所が正しくないか存在しません。');
+                    target.style.display='none';
+                    }
+                });
+            }
+            </script>
+            <script async defer
+	            src="https://maps.googleapis.com/maps/api/js?language=ja&region=JP&key=AIzaSyBgS57G9eQaTG16CwXb9rZfTXYLlOJHv6c&callback=initMap">
+	        </script>
             <div class="station">
                 <h3>最寄駅</h3>
                 <p>{!! nl2br(e($store->station)) !!}</p>
