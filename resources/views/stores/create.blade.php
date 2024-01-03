3c536cfabc7c0074455a894e8d7d78996c00615c
koki428 <koki.kikkawa@gmail.com>
2024-01-04 00:42:12 +0900

    編集の入力を全てtextareaにした

--- resources/views/stores/create.blade.php
+++ resources/views/stores/create.blade.php
@@ -10,31 +10,31 @@
             @csrf
             <div class="store_name">
                 <h2>店名</h2>
-                <input type="text" name="store[name]" placeholder="店名を入力"/>
+               <textarea name="store[name]" placeholder="店名を入力"></textarea>
             </div>
             <div class="hotpeppar">
                 <h2>ホットペッパー</h2>
-                <input type="text" name="store[hotpeppar]" placeholder="ホットペッパーの評価を入力"/>
+                <textarea name="store[hotpeppar]" placeholder="ホットペッパーの評価を入力"></textarea>
             </div>
             <div class="tabelog">
                 <h2>食べログ</h2>
-                <input type="text" name="store[tabelog]" placeholder="食べログの評価を入力"/>
+                <textarea name="store[tabelog]" placeholder="食べログの評価を入力"></textarea>
             </div>
             <div class="Google">
                 <h2>Google</h2>
-                <input type="text" name="store[google]" placeholder="Googleの評価を入力"/>
+                <textarea name="store[google]" placeholder="Googleの評価を入力"></textarea>
             </div>
             <div class="average">
                 <h2>平均</h2>
-                <input type="text" name="store[average]" placeholder="３つの評価の平均を入力"/>
+                <textarea name="store[average]" placeholder="3つの評価の平均を入力"></textarea>
             </div>
             <div class="address">
                 <h2>住所</h2>
-                <input type="text" name="store[address]" placeholder="住所を入力"/>
+                <textarea name="store[address]" placeholder="住所を入力"></textarea>
             </div>
             <div class="tell">
                 <h2>電話番号</h2>
-                <input type="text" name="store[tell]" placeholder="電話番号を入力"/>
+                <textarea name="store[tell]" placeholder="電話番号を入力"></textarea>
             </div>
             <div class="time">
                 <h2>営業時間</h2>
@@ -42,7 +42,7 @@
             </div>
             <div class="ジャンル">
                 <h2>ジャンル</h2>
-                <input type="text" name="store[genre]" placeholder="ジャンルを入力"/>
+                <textarea name="store[genre]" placeholder="ジャンルを入力"></textarea>
             </div>
             <div class="pay">
                 <h2>支払い方法</h2>
@@ -50,7 +50,7 @@
             </div>
             <div class="close">
                 <h2>定休日</h2>
-                <input type="text" name="store[close]" placeholder="定休日を入力"/>
+                <textarea name="store[close]" placeholder="定休日を入力"></textarea>
             </div>
             <div class="station">
                 <h2>最寄駅</h2>
